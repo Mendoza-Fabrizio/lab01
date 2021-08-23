533573248db92c7d54e6002bc15a1e41c02cb422
sebastianalpaca <82391702+sebastianalpaca@users.noreply.github.com>
2021-08-23 00:37:31 -0500

    Add files via upload
    experiencia 3

---
+++ WebForm1.aspx.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication3
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            {
+                initPositionRol();
+
+            }
+        }
+
+        private void initPositionRol()
+        {
+            Rol.Items.Clear();
+            Rol.Enabled = true;
+            Rol.Items.Add("Analista");
+            Rol.Items.Add("Diseñador");
+            Rol.Items.Add("Desarrollador");
+        }
+
+        protected void posicion_SelectedIndexChanged(object sender, EventArgs e)
+
+            {
+                if (posicion.Items[0].Selected == true)
+                    initPositionRol();
+                if (posicion.Items[1].Selected == true)
+                {
+                    Rol.Items.Clear();
+                    Rol.Enabled = true;
+                    Rol.Items.Add("Administrador General");
+                    Rol.Items.Add("Administrador de Proyecto");
+                }
+                if (posicion.Items[2].Selected == true)
+                {
+                    Rol.Items.Clear();
+                    Rol.Enabled = true;
+                    Rol.Items.Add("VP Ventas");
+                    Rol.Items.Add("VP Marketing");
+                    Rol.Items.Add("VP Producción");
+                    Rol.Items.Add("VP Recursos Humanos");
+                }
+                if (posicion.Items[32].Selected == true)
+                {
+                    Rol.Items.Clear();
+                    Rol.Enabled = false;
+                }
+
+            }
+
+        protected void grabar_Click(object sender, EventArgs e)
+        {
+            string posicio = "";
+     if (posicion.SelectedValue == "Trabajador")
+                posicio = "Trabajador";
+     if (posicion.SelectedValue == "Jefe")
+                posicio = "Jefe";
+     if (posicion.SelectedValue == "Vicepresidente")
+                posicio = "Vicepresidente";
+     if (posicion.SelectedValue == "Presidente")
+                posicio = "Presidente";
+            infolabel.Text = " Empleado: " + Nombre.Text +" "+ Apellido.Text +
+     " Codigo: " + Codigo.Text + " Posición: " + posicio;
+            posicion.SelectedIndex = 0;
+        }
+        protected void Limpiar_Click(object sender, EventArgs e)
+        {
+            Nombre.Text = "";
+            Apellido.Text = "";
+            Codigo.Text = "";
+            initPositionRol();
+            posicion.SelectedIndex = 0;
+            infolabel.Text = "";
+        }
+    }
+
+}
---
+++ WebForm1.aspx.designer.cs
@@ -0,0 +1,152 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace WebApplication3
+{
+
+
+    public partial class WebForm1
+    {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control Label1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+
+        /// <summary>
+        /// Control label2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label label2;
+
+        /// <summary>
+        /// Control Nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Nombre;
+
+        /// <summary>
+        /// Control Label3.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label3;
+
+        /// <summary>
+        /// Control Apellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Apellido;
+
+        /// <summary>
+        /// Control Label4.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label4;
+
+        /// <summary>
+        /// Control Codigo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Codigo;
+
+        /// <summary>
+        /// Control Label6.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label6;
+
+        /// <summary>
+        /// Control Rol.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList Rol;
+
+        /// <summary>
+        /// Control Label5.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label5;
+
+        /// <summary>
+        /// Control posicion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RadioButtonList posicion;
+
+        /// <summary>
+        /// Control grabar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button grabar;
+
+        /// <summary>
+        /// Control limpiar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button limpiar;
+
+        /// <summary>
+        /// Control infolabel.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label infolabel;
+    }
+}
